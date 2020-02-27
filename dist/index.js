f347408dfc342613c8e9f908bd0aafd25db45a7b
Sebastian Kade <sebastian@bitbound.com.au>
2020-02-27 18:21:26 +0900

    Add convenience index

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// Cleaner syntax coming in 3.8
+// https://devblogs.microsoft.com/typescript/announcing-typescript-3-8-beta/
+Object.defineProperty(exports, "__esModule", { value: true });
+const date = require("./date");
+exports.date = date;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+// Cleaner syntax coming in 3.8
+// https://devblogs.microsoft.com/typescript/announcing-typescript-3-8-beta/
+
+import * as currency from "./currency";
+import * as id from "./id";
+import * as object from "./object";
+import * as array from "./array";
+import * as env from "./env";
+import * as profile from "./profile";
+import * as time from "./time";
+import * as date from "./date";
+import * as fn from "./fn";
+import * as logic from "./logic";
+import * as promise from "./promise";
+import * as cache from "./cache";
+import * as definetly from "./definetly";
+import * as maybe from "./maybe";
+import * as timezone from "./timezone";
+import * as sql from "./sql";
+
+export {
+  currency,
+  id,
+  object,
+  array,
+  env,
+  profile,
+  time,
+  date,
+  fn,
+  logic,
+  promise,
+  cache,
+  definetly,
+  maybe,
+  timezone,
+  sql,
+};

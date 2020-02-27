49a78c43db6989d58b919d4763359a01bb112d74
Sebastian Kade <sebastian@bitbound.com.au>
2020-02-27 18:24:02 +0900

    add rest of files to index

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,17 @@
+import * as currency from "./currency";
+import * as id from "./id";
+import * as object from "./object";
+import * as array from "./array";
+import * as env from "./env";
+import * as profile from "./profile";
+import * as time from "./time";
 import * as date from "./date";
-export { date };
+import * as fn from "./fn";
+import * as logic from "./logic";
+import * as promise from "./promise";
+import * as cache from "./cache";
+import * as definetly from "./definetly";
+import * as maybe from "./maybe";
+import * as timezone from "./timezone";
+import * as sql from "./sql";
+export { currency, id, object, array, env, profile, time, date, fn, logic, promise, cache, definetly, maybe, timezone, sql, };
--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,36 @@
 // Cleaner syntax coming in 3.8
 // https://devblogs.microsoft.com/typescript/announcing-typescript-3-8-beta/
 Object.defineProperty(exports, "__esModule", { value: true });
+const currency = require("./currency");
+exports.currency = currency;
+const id = require("./id");
+exports.id = id;
+const object = require("./object");
+exports.object = object;
+const array = require("./array");
+exports.array = array;
+const env = require("./env");
+exports.env = env;
+const profile = require("./profile");
+exports.profile = profile;
+const time = require("./time");
+exports.time = time;
 const date = require("./date");
 exports.date = date;
+const fn = require("./fn");
+exports.fn = fn;
+const logic = require("./logic");
+exports.logic = logic;
+const promise = require("./promise");
+exports.promise = promise;
+const cache = require("./cache");
+exports.cache = cache;
+const definetly = require("./definetly");
+exports.definetly = definetly;
+const maybe = require("./maybe");
+exports.maybe = maybe;
+const timezone = require("./timezone");
+exports.timezone = timezone;
+const sql = require("./sql");
+exports.sql = sql;
 //# sourceMappingURL=index.js.map

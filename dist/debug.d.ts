8eecf477a6c176b1d9097321f17da946aaa4de51
Sebastian Kade <sebastian@bitbound.com.au>
2020-02-27 18:21:30 +0900

    Remove old dist files

--- dist/debug.d.ts
+++
@@ -1,4 +0,0 @@
-import * as Sentry from "@sentry/node";
-export declare const debug: (...messages: any[]) => Maybe<void>;
-export declare const log: (message: string | Error | Record<string, any>, extra?: Record<string, any> | undefined) => void;
-export { Sentry };

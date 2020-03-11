9e6edc9bec471ebf1095f24d0bc588f34905c830
Sebastian Kade <sebastian@bitbound.com.au>
2020-03-11 17:34:24 +0900

    Build changes before package bump.

--- publish.sh
+++ publish.sh
@@ -2,9 +2,17 @@
 
 set -e;
 
-VER=$(npm version patch)
+npm run test
+
+npm run build
+
+set +e;
 git stage .
-git commit -m $VER
+git commit -m "Build changes before package bump."
+set -e;
+
+VER=$(npm version patch)
+
 git push origin
 git tag -a $VER
 git push origin $VER;

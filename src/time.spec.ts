ec51068ffb16c536d933d7a50c92f1593d5d5f93
Sebastian Kade <sebastian@bitbound.com.au>
2020-08-03 10:24:49 +1000

    Add more time utils
    - inFuture, inPast, isGreaterThan, isLessThan
    - add tests
    - extract out now into own file

--- src/time.spec.ts
+++ src/time.spec.ts
@@ -1,4 +1,5 @@
-import { toSeconds } from "./time";
+import { toSeconds, isGreaterThan, isLessThan, inPast, inFuture } from "./time";
+import { addDays, subDays } from "date-fns";
 
 test("should convert basic things", () => {
   expect(toSeconds("1 year")).toBe(1 * 365 * 24 * 60 * 60);
@@ -15,4 +16,24 @@ test("should convert basic things", () => {
   // Doesn't support months
   expect(() => toSeconds("1 month")).toThrow();
   expect(() => toSeconds("1 months")).toThrow();
+});
+
+test("date comparison", () => {
+  expect(isGreaterThan(new Date(), addDays(new Date(), 1))).toBe(false);
+  expect(isGreaterThan(addDays(new Date(), 1), new Date())).toBe(true);
+
+  expect(isLessThan(new Date(), addDays(new Date(), 1))).toBe(true);
+  expect(isLessThan(addDays(new Date(), 1), new Date())).toBe(false);
+});
+
+test("inPast returns correctly", () => {
+  expect(inPast(new Date())).toBe(false);
+  expect(inPast(subDays(new Date(), 1))).toBe(true);
+  expect(inPast(addDays(new Date(), 1))).toBe(false);
+});
+
+test("inFuture returns correctly", () => {
+  expect(inFuture(new Date())).toBe(false);
+  expect(inFuture(subDays(new Date(), 1))).toBe(false);
+  expect(inFuture(addDays(new Date(), 1))).toBe(true);
 });
--- src/time.ts
+++ src/time.ts
@@ -13,4 +13,28 @@ export const toMilliSeconds = (time: string): number => toSeconds(time) * 100;
 
 export const toMinutes = (seconds: number): number => Math.round(seconds / 60);
 
-export const now = () => new Date();
+import { now } from "./now";
+
+export * from "./now";
+
+export const ensureUTC = () => {
+  if (process.env.TZ !== "UTC") {
+    throw new Error("Should be running in UTC.");
+  }
+};
+
+const round = (num: number) => {
+  const rounded = Math.floor(Math.abs(num));
+  return `${rounded < 10 ? "0" : ""}${rounded}`;
+};
+
+export const formatOffsetInUTC = (offset: number) =>
+  `${offset <= 0 ? "+" : "-"}${round(offset / 60)}${round(offset % 60)}`;
+
+export const isLessThan = (timeA: Date, timeB: Date) =>
+  timeA.getTime() < timeB.getTime();
+export const isGreaterThan = (timeA: Date, timeB: Date) =>
+  timeA.getTime() > timeB.getTime();
+
+export const inFuture = (timeA: Date) => isGreaterThan(timeA, now());
+export const inPast = (timeA: Date) => isLessThan(timeA, now());

33d1c8075920ffc64bc3685fd650c0acb3f1efba
Sebastian Kade <sebastian@bitbound.com.au>
2020-09-04 18:27:54 +1000

    Build changes before package bump.

--- dist/sql.js
+++ dist/sql.js
@@ -2,42 +2,80 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const change_case_1 = require("change-case");
 const lodash_1 = require("lodash");
-const date_fns_1 = require("date-fns");
 const pgEscape = require("pg-escape");
+const date_fns_1 = require("date-fns");
 const array_1 = require("./array");
 exports.column = (name) => change_case_1.snakeCase(name);
 exports.table = (name) => change_case_1.snakeCase(name);
 exports.literal = (value) => {
+    // Treat null as NULL
     if (value === null) {
         return "NULL";
     }
+    // Treat undefined as empty
     if (value === undefined) {
         return "";
     }
+    // Format dates in UTC
     if (lodash_1.isDate(value)) {
-        return `'${date_fns_1.format(value, "YYYY-MM-DD HH:mm:ss.SSSZZ")}'`; // Format in UTC
+        return `'${date_fns_1.format(value, "YYYY-MM-DD HH:mm:ss.SSSZZ")}'`;
     }
     if (typeof value === "string") {
         return pgEscape.literal(value);
     }
+    if (lodash_1.isArray(value)) {
+        return `[${value.map(exports.literal).join(",")}]`;
+    }
+    // JSON blobs
+    if (lodash_1.isObject(value)) {
+        return pgEscape.literal(JSON.stringify(value));
+    }
+    // Literal string value
     return value.toString();
 };
 exports.toSet = (update) => lodash_1.map(lodash_1.toPairs(update), ([key, value]) => `${exports.column(key)} = ${exports.literal(value)}`).join(", ");
+exports.toValues = (items, columns = uniqColumns(items)) => {
+    const extractValues = (i) => columns.map((c) => {
+        const v = lodash_1.get(i, c);
+        return v === undefined ? "DEFAULT" : exports.literal(v);
+    });
+    return items.map(i => `${exports.toArray(extractValues(i))}`).join(", ");
+};
+const uniqColumns = (items) => lodash_1.keys(lodash_1.reduce(items, (acc, i) => (Object.assign(Object.assign({}, acc), i)), {}));
+exports.toColumns = (items) => {
+    // Map all items into one object to get union of fields
+    return `${exports.toArray(uniqColumns(items).map(exports.column))}`;
+};
 exports.toArray = (items) => `(${items.join(", ")})`;
-exports.toValues = (item) => exports.toArray(lodash_1.values(item).map(exports.literal));
-exports.toColumns = (item) => exports.toArray(lodash_1.keys(item).map(exports.column));
-exports.insert = (table, items) => {
-    const allItems = array_1.ensureArray(items);
+exports.toLiteralArray = (items) => exports.toArray(items.map(exports.literal));
+const formatReturning = (fields) => fields && !lodash_1.isEmpty(fields)
+    ? `RETURNING ${array_1.ensureArray(fields).join(", ")}`
+    : "";
+exports.insert = (table, items, returnFields) => {
+    const all = array_1.ensureArray(items);
+    if (lodash_1.isEmpty(items)) {
+        throw new Error("Can't insert empty array.");
+    }
+    const columns = uniqColumns(all);
     return `
-    INSERT INTO ${table} ${allItems.map(exports.toColumns)}
-    VALUES ${allItems.map(exports.toValues)}
+    INSERT INTO ${table} ${exports.toArray(columns.map(exports.column))}
+    VALUES ${exports.toValues(all, columns)}
+    ${formatReturning(returnFields)}
   `;
 };
-exports.upsert = (table, items, onConflictKeys, updateKeys) => `
-  ${exports.insert(table, array_1.ensureArray(items))}
-  ON CONFLICT ${exports.toArray(array_1.ensureArray(onConflictKeys).map(exports.column))} DO
-  UPDATE SET ${lodash_1.without(array_1.ensureArray(updateKeys), "updated_at")
-    .map(k => `${exports.column(k)} = excluded.${exports.column(k)}`)
-    .join(", ")}, updated_at = now()
-`;
+exports.upsert = (table, items, onConflictKeys, updateKeys, returnFields) => {
+    const all = array_1.ensureArray(items);
+    if (lodash_1.isEmpty(items)) {
+        throw new Error("Can't upsert empty array.");
+    }
+    return `
+    INSERT INTO ${table} ${exports.toColumns(all)}
+    VALUES ${exports.toValues(all)}
+    ON CONFLICT ${exports.toArray(lodash_1.map(array_1.ensureArray(onConflictKeys), exports.column))} DO
+    UPDATE SET ${lodash_1.without(lodash_1.map(array_1.ensureArray(updateKeys), exports.column), "updated_at")
+        .map(k => `${k} = excluded.${k}`)
+        .join(", ")}, updated_at = now()
+    ${formatReturning(returnFields)}
+  `;
+};
 //# sourceMappingURL=sql.js.map
---
+++ dist/types.d.ts
@@ -0,0 +1,3 @@
+export declare type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export declare type Primitive = string | number | Date | boolean | undefined | null;
+export declare type PrimitiveRecord = Record<string, Primitive>;

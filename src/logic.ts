daec34ef5d4f6cfa9638684f74d41ee085e6fdef
Sebastian Kade <sebastian@bitbound.com.au>
2020-12-18 17:20:42 +1100

    Add ifDo

--- src/logic.ts
+++ src/logic.ts
@@ -1,1 +1,13 @@
+import { isFunction } from "lodash";
+import { Fn, Pred } from "./fn";
+
 export const not = <T>(func: (arg: T) => boolean) => (arg: T) => !func(arg);
+
+const ensureFn = <V>(v: V | Fn<void, V>) => (isFunction(v) ? v : () => v);
+
+export const ifDo = <T>(cond: boolean | Pred<void>, val: T | Fn<void, T>) => {
+  if (ensureFn(cond)()) {
+    return ensureFn(val)();
+  }
+  return undefined;
+};

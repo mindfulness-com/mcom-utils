1d0f12ee883298db06b31f1a8e7ed5c79fcca91b
Sebastian Kade <sebastian@bitbound.com.au>
2021-03-04 10:33:33 +1100

    Build changes before package bump.

--- dist/logic.d.ts
+++ dist/logic.d.ts
@@ -1,3 +1,3 @@
-import { Fn, Pred } from "./fn";
+import { Fn } from "./fn";
 export declare const not: <T>(func: (arg: T) => boolean) => (arg: T) => boolean;
-export declare const ifDo: <T>(cond: boolean | Pred<void>, val: T | Fn<void, T>) => T | undefined;
+export declare const ifDo: <T>(cond: boolean | Fn<void, boolean | undefined> | undefined, val: T | Fn<void, T>) => T | undefined;

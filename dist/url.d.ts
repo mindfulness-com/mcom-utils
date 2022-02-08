cab1d9ad2d7bbb3d241ac99c556c87de3dde588c
Sebastian Kade <sebastian@bitbound.com.au>
2022-02-09 10:24:29 +1100

    Build changes before package bump.

--- dist/url.d.ts
+++ dist/url.d.ts
@@ -1,9 +1,6 @@
 import { stringify } from "query-string";
 import { Maybe } from "./maybe";
-import { Fn } from "./fn";
 declare type Primitive = string | number | boolean;
 export declare const toQueryParams: typeof stringify;
 export declare const toQueryString: (params: Record<string, Maybe<Primitive>>) => string;
-export declare const hashParams: Fn<Record<string, Maybe<string | boolean | number>>, string>;
-export declare const dehashParams: (s: string) => Record<string, Maybe<Primitive>>;
 export {};

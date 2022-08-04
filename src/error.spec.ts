febb22a1357ed873b03ea954b201d937036fa8f7
George Butter <georgestuartbutter@gmail.com>
2022-08-04 17:31:07 +1000

    Pr/throw (#23)
    * feat: update node engines
    
    * chore: npm install
    
    * feat: throw util
    
    * fix: lint

---
+++ src/error.spec.ts
@@ -0,0 +1,42 @@
+import { assertError, throw_ } from "./error";
+import { Maybe } from "./maybe";
+
+test("throw_ should throw error message", () => {
+  expect(() => {
+    throw_("error");
+  }).toThrow("error");
+});
+
+test("undefined should be asserted to an error", () => {
+  let error: Maybe<Error>;
+  try {
+    throw_(undefined);
+  } catch (err) {
+    error = assertError(err);
+  }
+  console.log(error);
+  expect(error).toBeDefined();
+  expect(error?.message).toEqual("non-error of type undefined has been thrown");
+});
+
+test("string should be asserted to an error", () => {
+  let error: Maybe<Error>;
+  try {
+    throw_("Some error");
+  } catch (err) {
+    error = assertError(err);
+  }
+  expect(error).toBeDefined();
+  expect(error?.message).toEqual("Some error");
+});
+
+test("object should be asserted to an error", () => {
+  let error: Maybe<Error>;
+  try {
+    throw_({ message: "Some error" });
+  } catch (err) {
+    error = assertError(err);
+  }
+  expect(error).toBeDefined();
+  expect(error?.message).toEqual('{"message":"Some error"}');
+});
--- src/error.ts
+++ src/error.ts
@@ -1,4 +1,14 @@
 /**
+ * Functional helper for throwing an error.
+ * @param {error} e
+ * @example
+ * when(result.error, _throw);
+ */
+export const throw_ = <T>(e: T) => {
+  throw e;
+};
+
+/**
  * As of typescript 4.4.x thrown Errors are of unknown type so this function is used
  * to assert that the error is of the expected type and attempts to convert the error to the expected type.
  * @param {unkown} err - The error to assert.
@@ -10,7 +20,7 @@ export const assertError = (err: unknown): Error => {
   } else if (typeof err === "string") {
     return new Error(err);
   } else if (typeof err === "object") {
-    return new Error(err?.toString());
+    return new Error(JSON.stringify(err));
   }
-  return new Error("Non-Error has been thrown");
+  return new Error(`non-error of type ${typeof err} has been thrown`);
 };

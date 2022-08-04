05f791581d6b9730f9a79fdc2824cf674797e8d2
George Butter <threeninenineone@gmail.com>
2022-08-04 16:27:55 +1000

    feat: export error

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 // https://devblogs.microsoft.com/typescript/announcing-typescript-3-8-beta/
 
 import * as currency from "./currency";
+import * as error from "error";
 import * as id from "./id";
 import * as object from "./object";
 import * as array from "./array";
@@ -22,6 +23,7 @@ import * as sql from "./sql";
 
 export {
   currency,
+  error,
   id,
   object,
   debug,

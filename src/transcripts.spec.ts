9b41caf85b7a532606ab2a98ebd9c61e4f35e15d
Sebastian Kade <sebastian@bitbound.com.au>
2023-02-03 11:17:42 +1100

    WebVTT Transcript Parser

---
+++ src/transcripts.spec.ts
@@ -0,0 +1,61 @@
+import { parseWebVTT } from "./transcripts";
+
+test("parseWebVTT", () => {
+  expect(
+    parseWebVTT(`WEBVTT - Translation of that film I like
+Kind: captions
+Language: en-US
+Channel: CC1
+Station: Online ABC
+ProgramID: SH010855880000
+ProgramType: TV series
+ProgramName: Castle
+Title: Law & Murder
+Season: 3
+Episode: 19
+PublishDate: 2011-03-28
+ContentAdvisory: TV-14
+
+NOTE
+This translation was done by Kyle so that
+some friends can watch it with their parents.
+
+1
+00:02:15.000 --> 00:02:20.000 position:10%,line-left align:left size:35%
+- Ta en kopp varmt te.
+- Det är inte varmt.
+
+2
+00:02:20.000 --> 00:02:25.000 position:10%,line-left align:left size:35%
+- Har en kopp te.
+- Det smakar som te.
+
+NOTE This last line may not translate well.
+
+3
+00:02:25.000 --> 00:02:30.000
+Ta en kopp`),
+  ).toEqual([
+    {
+      id: "1",
+      timeStart: "00:02:15.000",
+      timeEnd: "00:02:20.000",
+      settings: { position: "10%,line-left", align: "left", size: "35%" },
+      text: "Ta en kopp varmt te.\\nDet är inte varmt.",
+    },
+    {
+      id: "2",
+      timeStart: "00:02:20.000",
+      timeEnd: "00:02:25.000",
+      settings: { position: "10%,line-left", align: "left", size: "35%" },
+      text: "Har en kopp te.\\nDet smakar som te.",
+    },
+    {
+      id: "3",
+      timeStart: "00:02:25.000",
+      timeEnd: "00:02:30.000",
+      settings: {},
+      text: "Ta en kopp",
+    },
+  ]);
+});
---
+++ src/transcripts.ts
@@ -0,0 +1,56 @@
+import { first, fromPairs, map, reduce } from "lodash";
+import { split } from "lodash/fp";
+
+import { fallback, using } from "./fn";
+import { ifDo_ } from "./logic";
+import { switchEnum } from "./logic";
+import { Maybe } from "./maybe";
+
+interface Cue {
+  id?: Maybe<string>;
+  timeStart: string;
+  timeEnd: string;
+  settings: Record<string, Maybe<string>>;
+  text: string;
+}
+
+const seperateParts = (raw: string) => raw.split(/\n\n/gi);
+
+const partType = (raw: string) =>
+  using([first(raw.split("\n"))], firstLine =>
+    fallback(
+      ifDo_(firstLine?.startsWith("WEBVTT"), () => "HEAD"),
+      ifDo_(firstLine?.startsWith("NOTE"), () => "NOTE"),
+      () => "CUE",
+    ),
+  );
+
+const parseCue = (raw: string) => {
+  const [first, second, ...rest] = raw.split(/\n/);
+  const hasID = first.includes("-->");
+  const timing = hasID ? first : second;
+  const id = !hasID ? first : undefined;
+
+  const [timeStart, _arrow, timeEnd, ...settings] = timing.split(" ");
+
+  return {
+    id,
+    timeStart,
+    timeEnd,
+    settings: fromPairs(map(settings, split(":"))),
+    text: map(rest, l => l.replace(/^- /gi, "")).join("\\n"),
+  };
+};
+
+export const parseWebVTT = (raw: string): Cue[] =>
+  reduce(
+    seperateParts(raw),
+    (r, p) =>
+      switchEnum(partType(p), {
+        CUE: () => [...r, parseCue(p)],
+        // NOTE: () => r, // Not supported atm
+        // HEAD: () => r, // Not supported atm
+        else: () => r,
+      }),
+    [] as Cue[],
+  );

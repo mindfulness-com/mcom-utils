c6983ae798bc8ae3edbbda3f9906fdfafcb41d08
George Butter <georgestuartbutter@gmail.com>
2023-07-13 12:12:39 +1000

    chore: build

--- dist/time.d.ts
+++ dist/time.d.ts
@@ -8,3 +8,9 @@ export declare const isLessThan: (timeA: Date, timeB: Date) => boolean;
 export declare const isGreaterThan: (timeA: Date, timeB: Date) => boolean;
 export declare const inFuture: (timeA: Date) => boolean;
 export declare const inPast: (timeA: Date) => boolean;
+/**
+ * Takes a date and sets the time to the end of the day.
+ * @param date - The date to set the time to the end of the day.
+ * @returns {Date} - The provided date, but with the time set to the end of the day.
+ */
+export declare const endOfDay: (date: Date) => Date;

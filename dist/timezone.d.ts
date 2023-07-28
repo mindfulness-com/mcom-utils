5cf31bb228d351ae64952057bf8cdbdf09936ae0
Steven Barlow <stemail23@gmail.com>
2023-07-28 18:15:55 +1000

    chore(deps): revert convert to date-fns-tz

--- dist/timezone.d.ts
+++ dist/timezone.d.ts
@@ -1,2 +1,3 @@
 export declare const setTimeZone: (time: Date, timeZone: string) => Date;
 export declare const inTimeZone: (time: Date, timeZone: string) => Date;
+export declare const formatInTimeZone: (date: Date, format: string, timeZone: string) => string;
--- src/timezone.ts
+++ src/timezone.ts
@@ -1,11 +1,22 @@
-import { zonedTimeToUtc, utcToZonedTime } from "date-fns-tz";
+import {
+  convertToLocalTime,
+  convertToTimeZone,
+  formatToTimeZone,
+} from "date-fns-timezone";
 
 // Takes the exisint UTC date and set the disired timezone
 // Used to take an absolute time (e.g. this launches on 02 Dec 00:00)
 // and "cast" it into a timezone.
 export const setTimeZone = (time: Date, timeZone: string): Date =>
-  zonedTimeToUtc(time, timeZone);
+  convertToLocalTime(time, { timeZone });
 
 // What time is it in the timezone
 export const inTimeZone = (time: Date, timeZone: string): Date =>
-  utcToZonedTime(time, timeZone);
+  convertToTimeZone(time, { timeZone });
+
+// What time is it in the timezone
+export const formatInTimeZone = (
+  date: Date,
+  format: string,
+  timeZone: string,
+): string => formatToTimeZone(date, format, { timeZone });

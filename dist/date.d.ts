50d4cee36fe0a50dad1c0e583cc7ca87ff94aeed
Steven Barlow <stemail23@gmail.com>
2023-11-15 10:37:41 +1100

    feat: add daysUntil function (#169)

--- dist/date.d.ts
+++ dist/date.d.ts
@@ -4,3 +4,9 @@ export declare const getUtcOffset: (date: Date, timezone: string) => number;
 export declare const isMorning: (date: Date) => boolean;
 export declare const isAfternoon: (date: Date) => boolean;
 export declare const isEvening: (date: Date) => boolean;
+/**
+ * Calculate the total number of full whole days between now and a given dateTime
+ * @param {Date} d - The dateTime value to calculate the daysUntil.
+ * @returns {number} - The whole number of full days between now and the
+ */
+export declare const daysUntil: (d: Date) => number;

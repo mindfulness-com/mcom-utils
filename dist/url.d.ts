aebb2dc828f6158fe0a27935ae617734231cc3c9
Steven Barlow <stemail23@gmail.com>
2023-11-28 11:14:21 +1100

    rebuild

--- dist/url.d.ts
+++ dist/url.d.ts
@@ -1,6 +1,8 @@
 import { stringify } from "query-string";
 import { Maybe } from "./maybe";
 type Primitive = string | number | boolean;
+export declare const baseUrl: (url: string) => string | undefined;
 export declare const toQueryParams: typeof stringify;
-export declare const toQueryString: (params: Record<string, Maybe<Primitive>>) => string;
+export declare const toQueryString: (params: Record<string, Maybe<Primitive | Primitive[]>>) => string;
+export declare const addQueryParam: (url: string, params: Record<string, Maybe<Primitive | Primitive[]>>) => string;
 export {};

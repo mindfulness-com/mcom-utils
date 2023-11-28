79e08c07aff00ef02e86ef4151bce518f8a908d2
Steven Barlow <stemail23@gmail.com>
2023-11-28 11:18:22 +1100

    feat: extended url functions (#172)

--- dist/url.js
+++ dist/url.js
@@ -1,9 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.toQueryString = exports.toQueryParams = void 0;
+exports.addQueryParam = exports.toQueryString = exports.toQueryParams = exports.baseUrl = void 0;
 const query_string_1 = require("query-string");
 const lodash_1 = require("lodash");
+const baseUrl = (url) => (0, lodash_1.first)(url.split(/[\#\?]/));
+exports.baseUrl = baseUrl;
 exports.toQueryParams = query_string_1.stringify;
-const toQueryString = (params) => (0, lodash_1.isEmpty)(params) ? "" : `?${(0, exports.toQueryParams)(params)}`;
+const toQueryString = (params) => ((0, lodash_1.isEmpty)(params) ? "" : `?${(0, exports.toQueryParams)(params)}`);
 exports.toQueryString = toQueryString;
+const addQueryParam = (url, params) => {
+    const [base, existing] = url.split("?");
+    return existing
+        ? `${base}?${existing}&${(0, exports.toQueryParams)(params)}`
+        : `${url}${(0, exports.toQueryString)(params)}`;
+};
+exports.addQueryParam = addQueryParam;
 //# sourceMappingURL=url.js.map
--- src/url.ts
+++ src/url.ts
@@ -1,11 +1,24 @@
 import { stringify } from "query-string";
-import { isEmpty } from "lodash";
+import { isEmpty, first } from "lodash";
 
 import { Maybe } from "./maybe";
 
 type Primitive = string | number | boolean;
 
+export const baseUrl = (url: string) => first(url.split(/[\#\?]/));
+
 export const toQueryParams = stringify;
 
-export const toQueryString = (params: Record<string, Maybe<Primitive>>) =>
-  isEmpty(params) ? "" : `?${toQueryParams(params)}`;
+export const toQueryString = (
+  params: Record<string, Maybe<Primitive | Primitive[]>>,
+) => (isEmpty(params) ? "" : `?${toQueryParams(params)}`);
+
+export const addQueryParam = (
+  url: string,
+  params: Record<string, Maybe<Primitive | Primitive[]>>,
+) => {
+  const [base, existing] = url.split("?");
+  return existing
+    ? `${base}?${existing}&${toQueryParams(params)}`
+    : `${url}${toQueryString(params)}`;
+};

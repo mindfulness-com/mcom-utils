f0945c34401e3d258f05338e35d3a573464feb07
Steven Barlow <stemail23@gmail.com>
2024-04-11 11:40:18 +1000

    recompile

--- dist/array.d.ts
+++ dist/array.d.ts
@@ -12,11 +12,11 @@ export declare const sortByInt: <T>(arr: T[], p: (t: T) => number) => T[];
 export declare const contains: <T>(arr: Maybe<T[]>, v: T) => boolean;
 export declare const containsAll: <T>(vals: T[], compare: T[]) => boolean;
 export declare const containsAny: <T>(vals: T[], compare: T[]) => boolean;
-export declare const pluckUnique: <T>(selector: (tag: T) => Maybe<string>) => (tags: T[]) => string[];
+export declare const pluckUnique: <T>(selector: (tag: T) => Maybe<string>) => ((tags: T[]) => string[]);
 export declare const indexBy: <T>(items: T[], pick: (i: T) => Maybe<string | string[]>) => {
     [key: string]: T;
 };
-export declare const lookup: <T>(items: T[], pick: (i: T) => Maybe<string | string[]>, fallback?: ((k: string) => T) | undefined) => (key: string) => NonNullable<T>;
+export declare const lookup: <T>(items: T[], pick: (i: T) => Maybe<string | string[]>, fallback?: (k: string) => T) => (key: string) => NonNullable<T>;
 export declare const maybeLookup: <T>(items: T[], pick: (i: T) => Maybe<string | string[]>) => Fn<string, Maybe<T>>;
 export declare const maybeMap: <T, R>(items: T[], map: (i: T) => Maybe<R>) => R[];
 export declare const omitEmpty: <T>(vals: Array<T | undefined | null>) => T[];

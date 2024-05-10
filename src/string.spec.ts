47c7e4077a1fe814f45a007961418b0f8d72e683
Steven Barlow <stemail23@gmail.com>
2024-05-10 12:11:53 +1000

    test with numbers

--- src/string.spec.ts
+++ src/string.spec.ts
@@ -16,20 +16,20 @@ import {
 describe("string", () => {
   describe("case conversion", () => {
     const testValues = {
-      camelCase: "twoWords",
-      capitalCase: "Two Words",
-      startCase: "Two Words",
-      titleCase: "Two Words",
-      dotCase: "two.words",
-      kebabCase: "two-words",
-      hyphenCase: "two-words",
-      noCase: "two words",
-      pascalCase: "TwoWords",
-      pascalSnakeCase: "Two_Words",
-      pathCase: "two/words",
-      sentenceCase: "Two words",
-      snakeCase: "two_words",
-      trainCase: "Two-Words",
+      camelCase: "twoWordsV2",
+      capitalCase: "Two Words V2",
+      startCase: "Two Words V2",
+      titleCase: "Two Words V2",
+      dotCase: "two.words.v2",
+      kebabCase: "two-words-v2",
+      hyphenCase: "two-words-v2",
+      noCase: "two words v2",
+      pascalCase: "TwoWordsV2",
+      pascalSnakeCase: "Two_Words_V2",
+      pathCase: "two/words/v2",
+      sentenceCase: "Two words v2",
+      snakeCase: "two_words_v2",
+      trainCase: "Two-Words-V2",
     };
 
     const doTest = (
@@ -56,44 +56,5 @@ describe("string", () => {
     describe("pathCase", doTest(pathCase, testValues.pathCase));
     describe("sentenceCase", doTest(sentenceCase, testValues.sentenceCase));
     describe("snakeCase", doTest(snakeCase, testValues.snakeCase));
-
-    describe("numbers in case change functions should not be interpreted as starting new words", () => {
-      test("camelCase", () => {
-        expect(camelCase("two words V2")).toBe("twoWordsV2");
-      });
-      test("capitalCase", () => {
-        expect(capitalCase("two words V2")).toBe("Two Words V2");
-      });
-      test("startCase", () => {
-        expect(startCase("two words V2")).toBe("Two Words V2");
-      });
-      test("titleCase", () => {
-        expect(titleCase("two words V2")).toBe("Two Words V2");
-      });
-      test("dotCase", () => {
-        expect(dotCase("two words V2")).toBe("two.words.v2");
-      });
-      test("kebabCase", () => {
-        expect(kebabCase("two words V2")).toBe("two-words-v2");
-      });
-      test("hyphenCase", () => {
-        expect(hyphenCase("two words V2")).toBe("two-words-v2");
-      });
-      test("noCase", () => {
-        expect(noCase("two words V2")).toBe("two words v2");
-      });
-      test("pascalCase", () => {
-        expect(pascalCase("two words V2")).toBe("TwoWordsV2");
-      });
-      test("pathCase", () => {
-        expect(pathCase("two words V2")).toBe("two/words/v2");
-      });
-      test("sentenceCase", () => {
-        expect(sentenceCase("two words V2")).toBe("Two words v2");
-      });
-      test("snakeCase", () => {
-        expect(snakeCase("two words V2")).toBe("two_words_v2");
-      });
-    });
   });
 });
